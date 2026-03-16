5fa7d8c458af840e361f4e1711f23ad3816d394a
AartiJoshi <Aarti Joshi>
2026-03-16 12:07:51 -0400

    fix: use same type in policy statement for_each branches

--- main.tf
+++ main.tf
@@ -16,21 +16,21 @@ locals {
 # Always create exactly one instance so count does not depend on unknown var.policy (e.g. from another resource).
 data "aws_iam_policy_document" "kms_policy" {
   dynamic "statement" {
-    for_each = local.policy_input_is_statement_map ? var.policy : {}
+    for_each = local.policy_input_is_statement_map ? toset(keys(var.policy)) : toset([])
     content {
-      sid       = statement.value.sid
-      effect    = statement.value.effect
-      actions   = statement.value.actions
-      resources = statement.value.resources
+      sid       = var.policy[statement.key].sid
+      effect    = var.policy[statement.key].effect
+      actions   = var.policy[statement.key].actions
+      resources = var.policy[statement.key].resources
       dynamic "principals" {
-        for_each = statement.value.principals
+        for_each = var.policy[statement.key].principals
         content {
           type        = principals.key
           identifiers = principals.value
         }
       }
       dynamic "condition" {
-        for_each = toset(lookup(statement.value, "condition", []))
+        for_each = toset(lookup(var.policy[statement.key], "condition", []))
         content {
           test     = condition.value.test
           variable = condition.value.variable
